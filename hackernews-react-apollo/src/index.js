0645a8e360f858f26324abe97fe6fd3e694dd93c
anton.miroshnichenko <anton.miroshnichenko@ix.co>
2019-12-04 21:06:36 +0300

    GraphQL Server

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import { ApolloProvider } from 'react-apollo';
+import { client } from './services/apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    ),
+    document.getElementById('root')
+);
